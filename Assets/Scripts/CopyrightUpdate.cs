6678d7aaa592e00d2830de591ba3640c4a53f3b4
UEJIN <sixthmirage1224@gmail.com>
2023-08-15 18:20:18 +0900

    pun2導入前
    pun2導入前

--- Assets/Scripts/CopyrightUpdate.cs
+++ Assets/Scripts/CopyrightUpdate.cs
@@ -1,13 +1,15 @@
 ﻿using UnityEngine;
 // Text を使用するため
 using UnityEngine.UI;
+using TMPro;
 
 public class CopyrightUpdate : MonoBehaviour
 {
     // 公開年
     [SerializeField] string year = string.Empty;
     // コピーライト表示欄
-    [SerializeField] Text copyright = null;
+    //[SerializeField] Text copyright = null;
+
 
     void Start()
     {
@@ -15,7 +17,22 @@ public class CopyrightUpdate : MonoBehaviour
         var version = Application.version;
         // リリース元
         var companyName = Application.companyName;
+
+
         // コピーライト書き換え
-        copyright.text = $"Ver.{version} \n © {year} {companyName}";
+        if (this.gameObject.GetComponent<TextMeshProUGUI>())
+        {
+           this.gameObject.GetComponent<TextMeshProUGUI>().text = $"Ver.{version} \n © {year} {companyName}";
+        }
+        else if (this.gameObject.GetComponent<Text>())
+        {
+           this.gameObject.GetComponent<Text>().text = $"Ver.{version} \n © {year} {companyName}";
+        }
+
+
+
+
+        //// コピーライト書き換え
+        //copyright.text = $"Ver.{version} \n © {year} {companyName}";
     }
 }
