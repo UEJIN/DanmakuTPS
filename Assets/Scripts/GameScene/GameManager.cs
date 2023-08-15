aebcdcdd767e9fc45193e62e065d1263d461c9af
UEJIN <sixthmirage1224@gmail.com>
2023-08-15 23:10:11 +0900

    プレイヤー名表示

---
+++ Assets/Scripts/GameScene/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun; //Photonサーバーの情報を使用するため
+
+public class GameManager : MonoBehaviourPunCallbacks //Photon viewやPunを使用するため
+{
+    [SerializeField] GameObject playerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PhotonNetwork.IsConnected) //サーバーに接続していたら
+        {
+            if (playerPrefab != null)
+            {
+                int randomPoint = Random.Range(-2, 2);
+                PhotonNetwork.Instantiate(playerPrefab.name, new Vector2(randomPoint,randomPoint), Quaternion.identity); //Photonを介した生成
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameScene/MovementController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    // Rigidbody2D コンポーネントを格納する変数
+    private Rigidbody2D rb;
+    // 自機の移動速度を格納する変数（初期値 5）
+    public float speed = 5;
+
+    private Vector2 velocity = Vector2.zero; //初期値ゼロ
+
+    // ゲームのスタート時の処理
+    void Start()
+    {
+        // Rigidbody2D コンポーネントを取得して変数 rb に格納
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // ゲーム実行中の繰り返し処理
+    void Update()
+    {
+        // 右・左のデジタル入力値を x に渡す
+        float x = Input.GetAxisRaw("Horizontal");
+        // 上・下のデジタル入力値 y に渡す
+        float y = Input.GetAxisRaw("Vertical");
+        // 移動する向きを求める
+        // x と y の入力値を正規化して direction に渡す
+        Vector2 direction = new Vector2(x, y).normalized;
+        // 移動する向きとスピードを代入する
+        // Rigidbody2D コンポーネントの velocity に方向と移動速度を掛けた値を渡す
+        //rb.velocity = direction * speed;
+        velocity = direction * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        if (velocity != Vector2.zero) //velocityに入力値があれば
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime); //MovePositionメソッドを呼び、移動させる値を渡す
+        }
+    }
+}
